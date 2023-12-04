1d6bec8055ddc76fcdef487dd9952a0a67492da8
gary-limo <84548257+gary-limo@users.noreply.github.com>
2023-12-04 00:16:19 -0500

    Update oil.py

--- oil.py
+++ oil.py
@@ -1,7 +1,7 @@
 import re
 
-# Define a regular expression pattern to match AutoSys job names and their commands
-pattern = r'insert_job:\s+(\w+)\s+job_type:\s+\w+\s+command:\s+(.*?)\s+(?:insert_job:|$)'
+# Define a regular expression pattern to match AutoSys job definitions
+pattern = r'insert_job:\s+(\w+)\s+(?:.*?command:\s+(.*?)\s+|$)'
 
 # Open the .oil file and read its contents
 with open('your_autosys_file.oil', 'r') as file:
@@ -15,8 +15,9 @@ autoSysJobs = []
 
 for match in matches:
     job_name = match[0].strip()
-    command = match[1].strip()
-    autoSysJobs.append({"job_name": job_name, "command": command})
+    command = match[1].strip() if len(match) > 1 else None  # Capture the command if present
+    if job_name and command:
+        autoSysJobs.append({"job_name": job_name, "command": command})
 
 # Print the extracted AutoSys job names and commands
 for job in autoSysJobs:
